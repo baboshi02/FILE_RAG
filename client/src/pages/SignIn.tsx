c27a606765a266502b05edcc343a306d8de1f73e
baboshi <drhameid@yahoo.com>
2026-01-05 06:34:27 +0200

    Added Basic Login And Signin pages

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,5 +1,38 @@
+import { Button, Label, TextInput } from "flowbite-react";
+
 const SignIn = () => {
-  return <div>Sign In</div>;
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <form className="flex max-w-md flex-col gap-4 border border-gray-200 rounded-2xl p-3 py-5 min-w-[20vw]">
+        <div>
+          <h2 className="text-blue-300 text-xl">Sign In</h2>
+          <div className="mb-2 block">
+            <Label htmlFor="email2">Your email</Label>
+          </div>
+          <TextInput
+            id="email2"
+            type="email"
+            placeholder="name@email.com"
+            required
+            shadow
+          />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="password2">Your password</Label>
+          </div>
+          <TextInput id="password2" type="password" required shadow />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="repeat-password">Repeat password</Label>
+          </div>
+          <TextInput id="repeat-password" type="password" required shadow />
+        </div>
+        <Button type="submit">Register new account</Button>
+      </form>
+    </div>
+  );
 };
 
 export default SignIn;
--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,5 +1,51 @@
-const Login = () => {
-  return <div>Login</div>;
+import { Button, Checkbox, Label, TextInput } from "flowbite-react";
+import { Link } from "react-router";
+
+export const Login = () => {
+  return (
+    <div className="h-screen flex justify-center items-center  ">
+      <form className="flex max-w-md flex-col gap-4 border p-3 py-5 rounded-2xl border-gray-200">
+        <h2 className="text-blue-300 text-xl">Login</h2>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="email2">Your email</Label>
+          </div>
+          <TextInput
+            id="email2"
+            type="email"
+            placeholder="name@email.com"
+            required
+            shadow
+          />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="password2">Your password</Label>
+          </div>
+          <TextInput id="password2" type="password" required shadow />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="username">Your username</Label>
+          </div>
+          <TextInput id="username" required shadow />
+        </div>
+        <div className="flex items-center gap-2">
+          <Checkbox id="agree" />
+          <Label htmlFor="agree" className="flex">
+            I agree with the&nbsp;
+            <Link
+              to="#"
+              className="text-cyan-600 hover:underline dark:text-cyan-500"
+            >
+              terms and conditions
+            </Link>
+          </Label>
+        </div>
+        <Button type="submit">Register new account</Button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
